9284015a4d964cd20cf4c61e82926492c7f78161
BertieWoo88 <050688V@gmail.com>
2023-02-17 13:50:39 +0300

    task3 corrected

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -3,3 +3,25 @@
 //* при N < 0, нужно вывести от N до -1
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Введите число N");
+int N = int.Parse (Console.ReadLine()!);
+int m = Math.Abs (N);
+int [] array = new int[m];
+int i = 1;
+if (N<0) 
+{
+    i = N;
+    N = -1;
+}    
+//for (int j = 0; i!=N; )
+for (int j=0; i<=N; i++)
+{
+    array[j]= i*i*i;
+    j++;
+}
+for (int j=0; j!=m; j++)
+{
+   Console.Write(array[j] +" "); 
+}
